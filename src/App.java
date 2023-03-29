14628c47348b095f4e97f60d9f7988421dddbb42
JB <jadon.binda@gmail.com>
2023-03-28 22:17:02 -0300

    Imersão Java: Aula 02

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -21,10 +23,19 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
       
         // exibir e manipular os dados
+        var generator = new StickerGenerator();
+
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("\u001b[43;1m\u001b[37;1m" + filme.get("title") + "\u001b[0m");
-            System.out.println("\u001b[45;1m\u001b[37;1m" + filme.get("image") + "\u001b[0m");
-            System.out.println(filme.get("imDbRating"));
+            String ImageURL = filme.get("image");
+            String titulo = filme.get("title");
+            String nomeArquivo = titulo + ".png";
+            InputStream inputStream = new URL(ImageURL).openStream();
+            generator.criaImagem(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            // System.out.println("\u001b[43;1m\u001b[37;1m" + titulo + "\u001b[0m");
+            // System.out.println("\u001b[45;1m\u001b[37;1m" + filme.get("image") + "\u001b[0m");
+            // System.out.println("\u001b[45;1m\u001b[37;1m" + filme.get("imDbRating") + "\u001b[0m");
             System.out.println();
         }
 
---
+++ src/StickerGenerator.java
@@ -0,0 +1,50 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class StickerGenerator {
+    
+    public void criaImagem(InputStream inputStream, String nomeArquivo) throws IOException {
+
+        // lê imagem
+        // InputStream inputStream = new FileInputStream(new File("images/TopMovies_7.jpg"));
+        // InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_7.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria nova imagem em memória com transparência e tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;        
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copia a imagem original pra nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, null, 0, 0);
+
+        // configurar a fonte
+        var fonte = new Font("Impact", Font.BOLD, 72);
+        graphics.setFont(fonte);
+        graphics.setColor(Color.YELLOW);
+
+        // escreve uma frase na nova imagem
+        String texto = "MANERO!";
+        FontMetrics medidasDaFonte = graphics.getFontMetrics();
+        Rectangle2D retangulo = medidasDaFonte.getStringBounds(texto, graphics);
+        int larguraDoTexto = (int) retangulo.getWidth();
+        int posicaoTexto = (largura - larguraDoTexto) / 2;
+        graphics.drawString(texto, posicaoTexto, altura + 125);
+
+        // grava a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File("saida/"+nomeArquivo));
+
+    }
+
+}
